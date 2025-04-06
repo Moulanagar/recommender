948e160979bd5c45947abdd832103353dd894830
Moulanagar <arhamequbal123@gmail.com>
2025-04-06 18:44:21 +0530

    some minor changes

--- app.py
+++ app.py
@@ -74,5 +74,9 @@ def recommend():
 def home():
     return "Freelancer Recommendation System is running!"
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
